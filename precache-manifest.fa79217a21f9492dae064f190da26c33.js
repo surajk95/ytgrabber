5648c4d71efac7f77420d00aaaf3254e22890644
surajk95 <sssurajsuraj@gmail.com>
2019-04-07 14:14:54 +0530

    Updates

---
+++ precache-manifest.fa79217a21f9492dae064f190da26c33.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5edfd0598b83fb7cea8ba5c763e34f9a",
+    "url": "/ytgrabber/static/media/bg.5edfd059.jpg"
+  },
+  {
+    "revision": "2b8c73296414e5d36ed1",
+    "url": "/ytgrabber/static/js/runtime~main.1c97cce3.js"
+  },
+  {
+    "revision": "12fedf70f509e840d05d",
+    "url": "/ytgrabber/static/js/main.fc086ba5.chunk.js"
+  },
+  {
+    "revision": "a36533ae65938741f383",
+    "url": "/ytgrabber/static/js/2.4f017e5b.chunk.js"
+  },
+  {
+    "revision": "12fedf70f509e840d05d",
+    "url": "/ytgrabber/static/css/main.59a011cf.chunk.css"
+  },
+  {
+    "revision": "98d31ae8a4a9830b04f1e5ac1fc915e4",
+    "url": "/ytgrabber/index.html"
+  }
+];
